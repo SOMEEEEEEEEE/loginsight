d0090c43834cb394b638a30c604ff38f39aeb639
SOMEEEEEEEEE <1002485476@qq.com>
2026-05-06 07:24:29 +0100

    refactor(platform): introduce messaging abstraction and reorganize platform modules

--- shared/config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Settings:
-    SQS_QUEUE_URL = os.getenv("SQS_QUEUE_URL")
-    POLL_INTERVAL = int(os.getenv("POLL_INTERVAL", 2))
-    BATCH_SIZE = int(os.getenv("BATCH_SIZE", 5))
-
-
-settings = Settings()
